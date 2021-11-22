38cae072fe027c096eabbaf1df6d7f8a7bb45fc2
thanoskat <thanoskat@gmx.com>
2021-11-22 03:26:13 +0200

    added userInfoRoute

---
+++ routes/userInfoRoute.js
@@ -0,0 +1,19 @@
+const router = require('express').Router()
+const mongoose=require("mongoose");
+const jwt = require('jsonwebtoken')
+const userModel = require('../models/userModel')
+const verifyAccessToken = require('../middleware/verifyAccessToken')
+const toId = mongoose.Types.ObjectId;
+
+router.get('/:userid', verifyAccessToken, async (req, res) => {
+  try {
+    const user = await userModel.findById(toId(req.params.userid))
+    console.log(user)
+    res.json(user)
+  }
+  catch(error) {
+    res.send(error)
+  }
+})
+
+module.exports = router
